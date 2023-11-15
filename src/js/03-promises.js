b83462e720d63131487842ad1853502a3d9e516c
Mykola Fedotov <fedotovns@gmail.com>
2023-11-15 20:29:32 +0100

    task 03 - done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const delay = parseInt(this.elements.delay.value);
+  const step = parseInt(this.elements.step.value);
+  const amount = parseInt(this.elements.amount.value);
+
+  for (let i = 1; i < amount; i++) {
+    createPromise(i, delay + (i - 1) * step)
+      .then(({ position, delay }) => {
+        iziToast.success({
+          title: `✅ Fulfilled promise ${position} in ${delay}ms`,
+        });
+      })
+      .catch(({ position, delay }) => {
+        iziToast.error({
+          title: `❌ Rejected promise ${position} in ${delay}ms`,
+        });
+      });
   }
 }
